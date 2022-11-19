7eaf1bc3aba171600c4b5b577a9269df21cc6094
Madhav2008 <aroramadhav65@gmail.com>
2022-11-19 23:20:58 +0530

    Update

--- lib/Screens/Intro_Screen.dart
+++ lib/Screens/Intro_Screen.dart
@@ -30,7 +30,7 @@ class IntroScreen extends StatelessWidget {
               child: Text(
                 'We deliver groceries at your doorstep',
                 textAlign: TextAlign.center,
-                style: GoogleFonts.notoSerif(
+                style: TextStyle(
                   fontSize: 36,
                   fontWeight: FontWeight.bold,
                 ),
