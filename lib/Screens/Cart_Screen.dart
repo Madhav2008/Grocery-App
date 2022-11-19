f87a8fe25d703c501a646459da9a28872216ad86
Madhav2008 <aroramadhav65@gmail.com>
2022-11-19 22:42:01 +0530

    Update

--- lib/Screens/Cart_Screen.dart
+++ lib/Screens/Cart_Screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:grocery_app/Models/Cart_Model.dart';
 import 'package:provider/provider.dart';
 
 import '../model/cart_model.dart';
