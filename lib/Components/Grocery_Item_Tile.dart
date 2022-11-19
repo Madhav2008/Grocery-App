c77829078965cfa701216dc4c59dc2ceb9948336
Madhav2008 <aroramadhav65@gmail.com>
2022-11-19 23:08:44 +0530

    Update

--- lib/Components/Grocery_Item_Tile.dart
+++ lib/Components/Grocery_Item_Tile.dart
@@ -23,7 +23,7 @@ class GroceryItemTile extends StatelessWidget {
       child: Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12),
-          color: color[200],
+          color: color[100],
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
