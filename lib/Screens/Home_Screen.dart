e27f6f643c30d0fda563b688f22fe2bedba6ed8c
Madhav2008 <aroramadhav65@gmail.com>
2022-11-19 22:49:49 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -77,7 +77,6 @@ class _HomePageState extends State<HomePage> {
 
           const SizedBox(height: 4),
 
-          // Let's order fresh items for you
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 24.0),
             child: Text(
