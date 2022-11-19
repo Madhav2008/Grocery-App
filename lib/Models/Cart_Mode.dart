e8f9ab251e77541ef88ae2c970aad4c9a7c629ff
Madhav2008 <aroramadhav65@gmail.com>
2022-11-19 22:38:33 +0530

    Update

--- lib/Models/Cart_Mode.dart
+++ lib/Models/Cart_Mode.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class CartModel extends ChangeNotifier {
   // list of items on sale
   final List _shopItems = const [
-    // [ itemName, itemPrice, imagePath, color ]
     ["Avocado", "4.00", "lib/images/avocado.png", Colors.green],
     ["Banana", "2.50", "lib/images/banana.png", Colors.yellow],
     ["Chicken", "12.80", "lib/images/chicken.png", Colors.brown],
