1af2fd64741f5102a81bab3d662fdcf427651fd4
Madhav2008 <aroramadhav65@gmail.com>
2022-11-19 22:26:47 +0530

    Update

--- lib/Components/Grocery_Item_Tile.dart
+++ lib/Components/Grocery_Item_Tile.dart
@@ -6,7 +6,7 @@ class GroceryItemTile extends StatelessWidget {
   final String itemName;
   final String itemPrice;
   final String imagePath;
-  final Color color;
+  final color;
   void Function()? onPressed;
 
   GroceryItemTile({
