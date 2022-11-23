6026c37ffb71763b5c55d25b8c821f48db22e92d
Madhav2008 <aroramadhav65@gmail.com>
2022-11-23 23:46:51 +0530

    Update

--- lib/Screens/Cart_Screen.dart
+++ lib/Screens/Cart_Screen.dart
@@ -40,7 +40,8 @@ class CartScreen extends StatelessWidget {
                     itemCount: value.cartItems.length,
                     padding: const EdgeInsets.all(12),
                     itemBuilder: (context, index) {
-                      return isLoading?
+                      return isLoading
+                      ?
                       isEmpty
                           ? Center(
                               child: Container(),
