a590ae40f68921a41ef438e945d127201c409dc3
Madhav2008 <aroramadhav65@gmail.com>
2022-11-23 23:20:14 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -38,6 +38,7 @@ class _HomeScreenState extends State<HomeScreen> {
           Padding(
             padding: const EdgeInsets.only(
               right: 24.0,
+              right: 24.0,
             ),
             child: Container(
               padding: const EdgeInsets.all(16),
