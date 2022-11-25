99886c1216ce3cf4ceeb91a9f1a849638ca95961
Madhav2008 <aroramadhav65@gmail.com>
2022-11-25 23:23:25 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -43,7 +43,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             child: Container(
               // padding: const EdgeInsets.all(10),
-              width: 30,
+              width: 50,
               height: 30,
               decoration: BoxDecoration(
                 color: Colors.black,
