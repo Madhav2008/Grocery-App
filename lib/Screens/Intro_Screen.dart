6a27f89c54d0fd06424d9801b4a0f36aedb5b722
Madhav2008 <aroramadhav65@gmail.com>
2022-11-25 22:42:34 +0530

    Update

--- lib/Screens/Intro_Screen.dart
+++ lib/Screens/Intro_Screen.dart
@@ -63,7 +63,9 @@ class IntroScreen extends StatelessWidget {
                 padding: const EdgeInsets.all(24),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(25),
-                  gradient: LinearGradient(colors: [],),
+                  gradient: LinearGradient(
+                    colors: [],
+                  ),
                 ),
                 child: const Center(
                   child: Text(
