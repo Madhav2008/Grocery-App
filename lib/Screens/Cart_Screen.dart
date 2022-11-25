0397b83a8f70b5ff92d4cdc25795b56cfbe24f3c
Madhav2008 <aroramadhav65@gmail.com>
2022-11-25 20:58:05 +0530

    Update

--- lib/Screens/Cart_Screen.dart
+++ lib/Screens/Cart_Screen.dart
@@ -76,7 +76,7 @@ class CartScreen extends StatelessWidget {
                                       ),
                                     ),
                                   ),
-                                )
+                                );
                           
                     },
                   ),
