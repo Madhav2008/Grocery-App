1a3ea128fe92702fa16b66f17e861b5f6c7f0b85
Madhav2008 <aroramadhav65@gmail.com>
2022-11-25 21:05:48 +0530

    Update

--- lib/Screens/Cart_Screen.dart
+++ lib/Screens/Cart_Screen.dart
@@ -41,7 +41,12 @@ class CartScreen extends StatelessWidget {
                     itemCount: value.cartItems.length,
                     padding: const EdgeInsets.all(12),
                     itemBuilder: (context, index) {
-                      return Padding(
+                      return isLoading
+                          ? isEmpty
+                              ? Lottie.network(
+                                  'https://assets10.lottiefiles.com/packages/lf20_awc77jfz.json',
+                                )
+                              : Padding(
                                   padding: const EdgeInsets.all(12.0),
                                   child: Container(
                                     decoration: BoxDecoration(
