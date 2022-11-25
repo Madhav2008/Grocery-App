db943a38c08302cc411b9f51a859c81806ba1f26
Madhav2008 <aroramadhav65@gmail.com>
2022-11-25 21:06:30 +0530

    Update

--- lib/Screens/Cart_Screen.dart
+++ lib/Screens/Cart_Screen.dart
@@ -35,18 +35,18 @@ class CartScreen extends StatelessWidget {
                 ),
               ),
               Expanded(
-                child: Padding(
+                child:isLoading
+                          ? isEmpty
+                              ? Lottie.network(
+                                  'https://assets10.lottiefiles.com/packages/lf20_awc77jfz.json',
+                                )
+                              :  Padding(
                   padding: const EdgeInsets.all(12.0),
                   child: ListView.builder(
                     itemCount: value.cartItems.length,
                     padding: const EdgeInsets.all(12),
                     itemBuilder: (context, index) {
-                      return isLoading
-                          ? isEmpty
-                              ? Lottie.network(
-                                  'https://assets10.lottiefiles.com/packages/lf20_awc77jfz.json',
-                                )
-                              : Padding(
+                      return Padding(
                                   padding: const EdgeInsets.all(12.0),
                                   child: Container(
                                     decoration: BoxDecoration(
