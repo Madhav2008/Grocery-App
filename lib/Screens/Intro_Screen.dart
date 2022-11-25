927fefe604f8c279cefd14a889a61a7fa4c5f968
Madhav2008 <aroramadhav65@gmail.com>
2022-11-25 23:11:29 +0530

    Update

--- lib/Screens/Intro_Screen.dart
+++ lib/Screens/Intro_Screen.dart
@@ -20,7 +20,7 @@ class IntroScreen extends StatelessWidget {
                 bottom: 20,
               ),
               child: Image.asset(
-                '../assets/images/intro4.png',
+                '../assets/images/intro1.png',
                 width: 500,
                 height: 300,
               ),
