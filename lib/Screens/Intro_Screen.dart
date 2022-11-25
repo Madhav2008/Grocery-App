2739a54511252e720df067c0282f499ee25d1bde
Madhav2008 <aroramadhav65@gmail.com>
2022-11-25 22:43:34 +0530

    Update

--- lib/Screens/Intro_Screen.dart
+++ lib/Screens/Intro_Screen.dart
@@ -66,6 +66,7 @@ class IntroScreen extends StatelessWidget {
                   gradient: const LinearGradient(
                     colors: [
                       Color(0xff4140b7),
+                      Color(0xff4140b7),
                     ],
                   ),
                 ),
