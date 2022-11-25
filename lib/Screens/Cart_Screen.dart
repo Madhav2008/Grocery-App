5ba0daea4d438c97490f11fc4e82d77b26baf480
Madhav2008 <aroramadhav65@gmail.com>
2022-11-25 21:07:04 +0530

    Update

--- lib/Screens/Cart_Screen.dart
+++ lib/Screens/Cart_Screen.dart
@@ -97,7 +97,7 @@ class CartScreen extends StatelessWidget {
                                   ),
                                 ),
                               ],
-                            )
+                            ),
               ),
               Padding(
                 padding: const EdgeInsets.all(36.0),
