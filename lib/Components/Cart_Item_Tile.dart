0ff3c37ddcacd879557852a8266362963ae527c2
Madhav2008 <aroramadhav65@gmail.com>
2022-11-26 23:14:01 +0530

    Update

--- lib/Components/Cart_Item_Tile.dart
+++ lib/Components/Cart_Item_Tile.dart
@@ -22,36 +22,35 @@ class CartItemTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: color[100],
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: ListTile(
-                            leading: Image.asset(
-                              value.cartItems[index][2],
-                              height: 36,
-                            ),
-                            title: Text(
-                              value.cartItems[index][0],
-                              style: GoogleFonts.arvo(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            subtitle: Text(
-                              '₹' + value.cartItems[index][1],
-                              style: GoogleFonts.arvo(fontSize: 12),
-                            ),
-                            trailing: IconButton(
-                              icon: const Icon(Icons.cancel),
-                              onPressed: () =>
-                                  Provider.of<CartModel>(context, listen: false)
-                                      .removeItemFromCart(index),
-                            ),
-                          ),
-                        ),
-                      );
+      padding: const EdgeInsets.all(12.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: color[100],
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: ListTile(
+          leading: Image.asset(
+            value.cartItems[index][2],
+            height: 36,
+          ),
+          title: Text(
+            value.cartItems[index][0],
+            style: GoogleFonts.arvo(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          subtitle: Text(
+            '₹' + value.cartItems[index][1],
+            style: GoogleFonts.arvo(fontSize: 12),
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.cancel),
+            onPressed: () => Provider.of<CartModel>(context, listen: false)
+                .removeItemFromCart(index),
+          ),
+        ),
+      ),
+    );
   }
 }
