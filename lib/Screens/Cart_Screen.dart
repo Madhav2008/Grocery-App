29b1329b49e5937eeb2e42c165135eb418e49c98
Madhav2008 <aroramadhav65@gmail.com>
2022-11-26 22:38:53 +0530

    Update

--- lib/Screens/Cart_Screen.dart
+++ lib/Screens/Cart_Screen.dart
@@ -57,7 +57,7 @@ class CartScreen extends StatelessWidget {
                             ),
                             title: Text(
                               value.cartItems[index][0],
-                              style: GoogleFonts.actor(
+                              style: GoogleFonts
                                 fontSize: 18,
                                 fontWeight: FontWeight.bold,
                               ),
