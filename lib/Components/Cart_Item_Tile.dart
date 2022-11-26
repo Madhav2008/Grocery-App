81cc7024c49641e93d3570005f6c09a47d7c9c4f
Madhav2008 <aroramadhav65@gmail.com>
2022-11-26 23:27:50 +0530

    Update

--- lib/Components/Cart_Item_Tile.dart
+++ lib/Components/Cart_Item_Tile.dart
@@ -6,7 +6,7 @@ import 'package:provider/provider.dart';
 class CartBody extends StatelessWidget {
   // final String itemName;
   // final String itemPrice;
-  final String value;
+  final value;
   final color;
   // void Function()? onPressed;
 
