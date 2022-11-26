17a58d43f0ec8b0a12b9bcfe65f2b4770102e1de
Madhav2008 <aroramadhav65@gmail.com>
2022-11-26 23:19:18 +0530

    Update

--- lib/Components/Cart_Item_Tile.dart
+++ lib/Components/Cart_Item_Tile.dart
@@ -3,7 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:grocery_app/Models/Cart_Model.dart';
 import 'package:provider/provider.dart';
 
-class CartItemTile extends StatelessWidget {
+class CartBody extends StatelessWidget {
   final String itemName;
   final String itemPrice;
   final String imagePath;
@@ -11,7 +11,7 @@ class CartItemTile extends StatelessWidget {
   final color;
   void Function()? onPressed;
 
-  CartItemTile({
+  CartBody({
     super.key,
     required this.itemName,
     required this.itemPrice,
@@ -24,125 +24,125 @@ class CartItemTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<CartModel>(
-        builder: (context, value, child) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                child: Text(
-                  "My Cart",
-                  style: GoogleFonts.arvo(
-                    fontSize: 36,
-                    fontWeight: FontWeight.bold,
-                  ),
+      builder: (context, value, child) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                "My Cart",
+                style: GoogleFonts.arvo(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: ListView.builder(
-                    itemCount: value.cartItems.length,
-                    padding: const EdgeInsets.all(12),
-                    itemBuilder: (context, index) {
-                      var color = value.shopItems[index];
-                      return Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: color[100],
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: ListTile(
-                            leading: Image.asset(
-                              value.cartItems[index][2],
-                              height: 36,
-                            ),
-                            title: Text(
-                              value.cartItems[index][0],
-                              style: GoogleFonts.arvo(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            subtitle: Text(
-                              '₹' + value.cartItems[index][1],
-                              style: GoogleFonts.arvo(fontSize: 12),
-                            ),
-                            trailing: IconButton(
-                              icon: const Icon(Icons.cancel),
-                              onPressed: () =>
-                                  Provider.of<CartModel>(context, listen: false)
-                                      .removeItemFromCart(index),
-                            ),
-                          ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: ListView.builder(
+                  itemCount: value.cartItems.length,
+                  padding: const EdgeInsets.all(12),
+                  itemBuilder: (context, index) {
+                    var color = value.shopItems[index];
+                    return Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: color[100],
+                          borderRadius: BorderRadius.circular(8),
                         ),
-                      );
-                    },
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(36.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    gradient: const LinearGradient(
-                      colors: [
-                        Color(0xff515acd),
-                        Color(0xff4140b7),
-                      ],
-                    ),
-                  ),
-                  padding: const EdgeInsets.all(24),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Total Price',
-                            style: TextStyle(color: Colors.green[200]),
+                        child: ListTile(
+                          leading: Image.asset(
+                            value.cartItems[index][2],
+                            height: 36,
                           ),
-                          const SizedBox(height: 8),
-                          Text(
-                            '\$${value.calculateTotal()}',
-                            style: const TextStyle(
+                          title: Text(
+                            value.cartItems[index][0],
+                            style: GoogleFonts.arvo(
                               fontSize: 18,
                               fontWeight: FontWeight.bold,
-                              color: Colors.white,
                             ),
                           ),
-                        ],
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.green.shade200),
-                          borderRadius: BorderRadius.circular(28),
-                        ),
-                        padding: const EdgeInsets.all(12),
-                        child: Row(
-                          children: const [
-                            Text(
-                              'Pay Now',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            Icon(
-                              Icons.arrow_forward_ios,
-                              size: 16,
-                              color: Colors.white,
-                            ),
-                          ],
+                          subtitle: Text(
+                            '₹' + value.cartItems[index][1],
+                            style: GoogleFonts.arvo(fontSize: 12),
+                          ),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.cancel),
+                            onPressed: () =>
+                                Provider.of<CartModel>(context, listen: false)
+                                    .removeItemFromCart(index),
+                          ),
                         ),
                       ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  gradient: const LinearGradient(
+                    colors: [
+                      Color(0xff515acd),
+                      Color(0xff4140b7),
                     ],
                   ),
                 ),
-              )
-            ],
-          );
-        },
-      );
+                padding: const EdgeInsets.all(24),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Total Price',
+                          style: TextStyle(color: Colors.green[200]),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          '\$${value.calculateTotal()}',
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.green.shade200),
+                        borderRadius: BorderRadius.circular(28),
+                      ),
+                      padding: const EdgeInsets.all(12),
+                      child: Row(
+                        children: const [
+                          Text(
+                            'Pay Now',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Icon(
+                            Icons.arrow_forward_ios,
+                            size: 16,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        );
+      },
+    );
   }
 }
