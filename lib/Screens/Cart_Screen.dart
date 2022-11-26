71fcb9d8e36b08614ec76c497f4731f7e0ccf0d5
Madhav2008 <aroramadhav65@gmail.com>
2022-11-26 23:33:45 +0530

    Update

--- lib/Screens/Cart_Screen.dart
+++ lib/Screens/Cart_Screen.dart
@@ -23,7 +23,7 @@ class CartScreen extends StatelessWidget {
         ),
       ),
       body: Consumer<CartModel>(
-        builder: (context, value, child,index) {
+        builder: (context, value,index) {
           return CartBody(
             value: value,
             color: value.shopItems[index][3],
