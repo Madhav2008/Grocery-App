992d824eec69c4cc5e8966f0b16ba8109edb2f8b
Madhav2008 <aroramadhav65@gmail.com>
2022-11-26 23:23:50 +0530

    Update

--- lib/Screens/Cart_Screen.dart
+++ lib/Screens/Cart_Screen.dart
@@ -22,13 +22,127 @@ class CartScreen extends StatelessWidget {
           color: Colors.grey[800],
         ),
       ),
-      body: CartBody(
-        // itemName: itemName,
-        // itemPrice: itemPrice,
-        // imagePath: imagePath,
-        color: value,
-        // onPressed: onPressed,
-      ),
+      body: return Consumer<CartModel>(
+      builder: (context, value, child) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                "My Cart",
+                style: GoogleFonts.arvo(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: ListView.builder(
+                  itemCount: value.cartItems.length,
+                  padding: const EdgeInsets.all(12),
+                  itemBuilder: (context, index) {
+                    var color = value.shopItems[index];
+                    return Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: color[100],
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: ListTile(
+                          leading: Image.asset(
+                            value.cartItems[index][2],
+                            height: 36,
+                          ),
+                          title: Text(
+                            value.cartItems[index][0],
+                            style: GoogleFonts.arvo(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: Text(
+                            '₹' + value.cartItems[index][1],
+                            style: GoogleFonts.arvo(fontSize: 12),
+                          ),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.cancel),
+                            onPressed: () =>
+                                Provider.of<CartModel>(context, listen: false)
+                                    .removeItemFromCart(index),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  gradient: const LinearGradient(
+                    colors: [
+                      Color(0xff515acd),
+                      Color(0xff4140b7),
+                    ],
+                  ),
+                ),
+                padding: const EdgeInsets.all(24),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Total Price',
+                          style: TextStyle(color: Colors.green[200]),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          '\$${value.calculateTotal()}',
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.green.shade200),
+                        borderRadius: BorderRadius.circular(28),
+                      ),
+                      padding: const EdgeInsets.all(12),
+                      child: Row(
+                        children: const [
+                          Text(
+                            'Pay Now',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Icon(
+                            Icons.arrow_forward_ios,
+                            size: 16,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        );
+      },
+    );
     );
   }
 }
